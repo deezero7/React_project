670e3774b30bb7a3d21aee6402cb7c84ba47f395
Deepndra singh <46988044+deezero7@users.noreply.github.com>
2025-06-14 13:40:27 +0530

    initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Hero = () => {
+  return (
+    <div className='relative h-dvh w-screen overflow-hidden flex items-center justify-center bg-gradient-to-b from-gray-800 to-gray-900 text-white'>
+        <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-950 inset-0">
+            <div className='mask-clip-path font-zentry'>
+        Hero is here wats up
+        </div>
+        </div>
+        </div>
+  )
+}
+
+export default Hero
