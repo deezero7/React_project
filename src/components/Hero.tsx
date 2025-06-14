b79b4b107d90904ee46babcd5699e1519e62c97c
Deepndra singh <46988044+deezero7@users.noreply.github.com>
2025-06-14 14:08:14 +0530

    small fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='relative h-dvh w-screen overflow-hidden flex items-center justify-center bg-gradient-to-b from-gray-800 to-gray-900 text-white'>
-        <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-950 inset-0">
-            <div className='mask-clip-path font-zentry'>
+    <div className=' relative h-dvh w-screen overflow-hidden flex items-center justify-center bg-gradient-to-b from-gray-800 to-gray-900 text-white'>
+        <div id="font-zentry video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-950 inset-0">
+            <div className='mask-clip-path font-zentry-regular relative h-dvh w-screen overflow-hidden flex items-center text-9xl justify-center text-white bg-gradient-to-b from-gray-800 to-gray-900 rounded-lg backdrop-blur-sm bg-opacity-50 p-4'>
         Hero is here wats up
         </div>
         </div>
